4083ae5d97cf872c94514c2cd604af5e90c05228
aleksplast <aleksplast6@gmail.com>
2022-08-23 18:42:12 +0300

    Try link
    Пытаюсь в линковку

--- quadratka.cpp
+++ quadratka.cpp
@@ -13,34 +13,29 @@ void square_equation(void);
 int main(void)
 {
     int test=1;
+
     printf("This program will solve quadratic equations with standard form of ax^2 + bx + c = 0\n");
 
     while(test == 1)
     {
-        fflush(stdin);
-        square_equation();
+        int roots = 0;
+        double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+
+        a = coeff(a);
+        b = coeff(b);
+        c = coeff(c);
+
+        assert(isfinite(a));
+        assert(isfinite(b));
+        assert(isfinite(c));
+
+        roots = solve_quadratic_equation(a,b,c, &x1, &x2);
+
+        output(roots, x1, x2);
         printf("Enter 1 to continue solving or 0 to end this program\n");
         scanf("%d", &test);
     }
     return 0;
-}
-
-void square_equation(void)
-{
-    int roots = 0;
-    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
-
-    a = coeff(a);
-    b = coeff(b);
-    c = coeff(c);
-
-    assert(isfinite(a));
-    assert(isfinite(b));
-    assert(isfinite(c));
-
-    roots = solve_quadratic_equation(a,b,c, &x1, &x2);
-
-    output(roots, x1, x2);
 }
 
 double coeff(double a)
